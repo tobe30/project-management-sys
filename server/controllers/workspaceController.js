5ed6e7eeaa5e645fa70daad378c8b9416666f09a
Tobe Marizu <tobemarizu@gmail.com>
2026-01-02 19:28:07 +0100

    problem

---
+++ server/controllers/workspaceController.js
@@ -0,0 +1,89 @@
+// Get all workspaces for user
+import prisma from "../configs/prisma.js";
+
+export const getUserWorkspaces = async (req, res) => {
+    try {
+        const { userId } = await req.auth();
+        const workspaces = await prisma.workspace.findMany({
+            where: {
+                members: {some: {userId: userId}}
+            },
+            include: {
+                members: {include: {user: true}},
+                projects: {
+                    include: {
+                        tasks: {include: {assignee: true, comments: {include: {
+                            user: true
+                        }}}},
+                        members: {include: {user: true}}
+                    }
+                },
+                owner: true
+            }
+        });
+        res.json(workspaces);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.code || error.message});
+    }
+}
+
+// Add member to workspace
+export const addMember = async (req, res) => {
+    try {
+        const { userId } = await req.auth();
+        const { email, role, workspaceId, message } = req.body;
+
+        // Check if the user exists
+        const user = await prisma.user.findUnique({
+            where: {email}
+        });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        if(!workspaceId || !role){
+            return res.status(400).json({ message: 'Missing required parameters' });
+        }
+
+        if(!["ADMIN", "MEMBER"].includes(role)){
+            return res.status(400).json({ message: 'Invalid role specified' });
+        }
+
+        // fetch workspace
+        const workspace = await prisma.workspace.findUnique({
+            where: {id: workspaceId}, include: {members: true}
+        })
+        if (!workspace) {
+            return res.status(404).json({ message: 'Workspace not found' });
+        }
+        
+        // check if creator has admin role
+        if(!workspace.members.find((member)=> member.userId === userId && member.role === 'ADMIN')){
+            return res.status(401).json({ message: 'Only admins can add members' });
+        }
+
+        // check if user is already a member
+        const existingMember = workspace.members.find((member)=> member.userId === userId)
+
+        if(existingMember){
+            return res.status(400).json({ message: 'User is already a member' });
+
+        }
+
+        const member = await prisma.workspaceMember.create({
+            data: {
+                userId: user.id,
+                workspaceId,
+                role,
+                message
+            }
+        })
+
+        res.json({member, message: 'Member added successfully' });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.code || error.message});
+    }
+}
--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express';
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
+import workspaceRouter from './routes/workspaceRoutes.js';
+import { protect } from './middleware/authMiddleware.js';
 
 const app = express();
 
@@ -15,6 +17,10 @@ app.get('/', (req, res)=> res.send('Server is running'));
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
+// Routes
+app.use("/api/workspaces", protect, workspaceRouter)
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
